4a6e1030f8a073e9642fc9a39a9aced8e186c4bf
c0descrub <jordanjames1095@googlemail.com>
2021-10-07 16:42:41 +0100

    Cool

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import UserControls from "./components/UserControls";
+import Header from "./components/Header";
+import Tasks from './components/Tasks';
+
 
 function App() {
+  const [tasks, setTasks] = useState([])
+
+//Add task
+  const addTask = (task)=> {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const completed = false
+    const newTask = { id, completed, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+  //Task counter
+  const TaskCount = () => {
+    return (<p>You have {tasks.length} {tasks.length === 1 ? 'task' : 'tasks'} </p>)
+    
+  }
+
+//Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+// Complete task
+ const completeTask = (id) => {
+  setTasks(tasks.map(task => {
+    if(task.id === id){
+      return{...task, completed: !task.completed}
+    }
+    return task
+  }))
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <TaskCount />
+      {tasks.length > 0 ? <Tasks setTasks={setTasks} tasks={tasks} deleteTask={deleteTask} completeTask={completeTask}/> : 'You\'re all done!'}
+      <UserControls onAdd={addTask} setTasks={setTasks}/>
     </div>
   );
 }

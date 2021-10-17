f6418ce24f80a7b1f2d8a9439b838a8ba13a91bf
c0descrub <jordanjames1095@googlemail.com>
2021-10-17 02:11:13 +0100

    Added 3rd party toggle component.

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,9 +1,43 @@
+import { ToggleSlider }  from "react-toggle-slider";
+
 const Toggle = () => {
+
+    const body = document.body
+    const darkMode = "dark"
+    const lightMode = "light"
+    let theme
+
+    theme = localStorage.getItem('Theme')
+
+    if (theme === lightMode || theme === darkMode){
+        body.classList.add(theme)
+    } else {
+        body.classList.replace(darkMode , lightMode)
+    }
+
+    const toggleTheme = () => {
+        if(body.classList.contains('dark')){
+            body.classList.remove('dark')
+            localStorage.setItem('Theme', "light")
+            theme = lightMode
+        } else {
+            body.classList.add('dark')
+            localStorage.setItem('Theme', "dark")
+            theme = darkMode
+        }
+    }
     return (
-        <label className="switch">
-            <input type="checkbox" id = "checkbox"/>
-            <span id="slider"></span>
-        </label>
+        <div className = "toggle-container">
+            <ToggleSlider 
+            active={theme === "dark" ? true : false}
+            barWidth={60} 
+            barBackgroundColor={'#0C0B22'}
+            barBackgroundColorActive={'#dedede'}
+            transitionDuration={'400ms'}
+            draggable={false}
+            onToggle={toggleTheme}
+            />
+        </div>
     )
 }
 
